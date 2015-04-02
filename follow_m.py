a321798d0320d76a0141ed7a9b52f8b3a3ff5c43
xchenmeow <chenxy12345@yahoo.com>
2015-04-02 12:07:29 +0800

    add all features to decision tree

--- follow_m.py
+++ follow_m.py
@@ -13,8 +13,9 @@ train_df = train_df[train_df['revenue']<10000000]
 train_df = train_df.replace(0, np.nan)
 train_df = train_df.dropna(axis=1,thresh=80)
 train_df = train_df.replace('IL',0)
-train_df = train_df.replace('FC',0)
-train_df = train_df.replace('DT',0)
+train_df = train_df.replace('FC',1)
+train_df = train_df.replace('DT',2)
+train_df['City Group'] = train_df['City Group'].map({'Big Cities': 0, 'Other': 1})
 train_df['Open_Year'] = train_df['Open Date'].map( lambda x : x.year)
 # c_year = train_df['City']
 # c_year[train_df['Open_Year']<2007] = u'b'
@@ -34,11 +35,34 @@ predicty = clf.predict(X.values)
 errors = predicty - train_df['revenue']
 RMSE = math.sqrt(np.mean(errors ** 2))
 print RMSE
-pylab.hist(errors,bins=20)
-pylab.title('errors histogram')
-pylab.xlabel('error')
-pylab.ylabel('count')
-pylab.show()
+# pylab.hist(errors,bins=20)
+# pylab.title('errors histogram')
+# pylab.xlabel('error')
+# pylab.ylabel('count')
+# pylab.show()
 
+
+loo = LeaveOneOut(50)
+# features = ['Open_Year', 'Type']
+features = [column for column in train_df.columns if column != 'City' and column != 'Open Date']
+test_1_predicts = []
+errs = []
+for train_idx, test_idx in loo:
+    train_1 = train_df.iloc[ train_idx ]
+    test_1 = train_df.iloc[ test_idx ]
+    X = train_1[ features ]
+    x_1 = test_1[ features ]
+    clf = tree.DecisionTreeRegressor()
+    clf = clf.fit(X.values, train_1['revenue'])
+    y_1_hat = clf.predict(x_1)
+    test_1_predicts.append(y_1_hat)
+    
+    y_1 = test_1.revenue.iloc[0]
+    errs.append(y_1_hat - y_1)
+
+pylab.hist(pd.Series(data=errs), bins=20)
+pylab.show()
+test_rmse = math.sqrt(np.mean(pd.Series(data=errs) ** 2))
+print test_rmse
 
 
